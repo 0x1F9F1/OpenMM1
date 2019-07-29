25cfe87753ce8662486c15084c814975b5408c6f
Brick <6098371+0x1F9F1@users.noreply.github.com>
2019-07-29 15:57:28 +0100

    cstring_t: Removed redundant check

--- src/data7/cstr.h
+++ src/data7/cstr.h
@@ -74,7 +74,7 @@ struct cstring_t
     inline void assign(const char* value)
     {
         delete[] m_pData;
-        m_pData = value ? StringDuplicate(value) : nullptr;
+        m_pData = StringDuplicate(value);
     }
 
     inline operator char*() noexcept
